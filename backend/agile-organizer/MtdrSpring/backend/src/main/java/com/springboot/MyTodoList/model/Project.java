948e306d31656de8ff522b35dafc1c32ba9abef1
root <emilianoomar65@gmail.com>
2025-03-05 11:27:15 -0600

    added models

--- backend/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/Project.java
+++ backend/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/Project.java
@@ -1,3 +1,17 @@
-public class Project {
+package com.springboot.MyTodoList.model;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project extends BaseEntity{
+
+    private String name;
+    private String description;
+    private int workflow_priority;
+    private OffsetDateTime dueDate;
+    private State state;
     
+
 }
--- backend/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/Team.java
+++ backend/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/Team.java
@@ -4,12 +4,10 @@ import javax.persistence.*;
 import java.time.OffsetDateTime;
 
 @Entity
-@Table(name = "STATE")
-public class State extends BaseEntity{
-
+@Table(name = "TEAM")
+public class Team extends BaseEntity{
     private String name;
     private String description;
-    private int workflow_priority;
 
 
 }
