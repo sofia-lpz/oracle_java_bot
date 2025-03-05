440dd2d32b8a680ed51718d33b9282cbf3bd4fd5
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2025-03-05 11:22:52 -0600

    NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
 import { useState } from 'react';
-import { Button } from 'antd'; // Importa el botón de Ant Design
-import NavigationMenu from './components/NavigationMenu'; // Importa el menú de navegación
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React + Ant Design</h1>
-      <div className="card">
-        <Button type="primary" onClick={() => setCount(count + 1)}>
-          Count is {count}
-        </Button>
-      </div>
-      {/* Agrega el menú de navegación aquí */}
-      <NavigationMenu />
-    </>
+      <NavBar />
   );
 }
 
