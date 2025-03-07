e8cd223308ea200ac09c88e12aa01ede535784ad
sofia-lpz <a01028251@tec.mx>
2025-03-07 00:47:41 -0600

    los cambios que hizo edgar pero en el dir integracion

--- integration/agile-organizer/MtdrSpring/backend/src/main/frontend/src/pages/Task.jsx
+++ integration/agile-organizer/MtdrSpring/backend/src/main/frontend/src/pages/Task.jsx
@@ -9,19 +9,19 @@ const Task = () => {
       <div className="kanban">
         <div>
           <h2>To Do</h2>
-          <div className="kanban-column">
+          <div className="kanban-column kanban-scroll">
             <TaskCard/>
           </div>
         </div>
         <div>
           <h2>In Progress</h2>
-          <div className="kanban-column">
+          <div className="kanban-column kanban-scroll">
             <TaskCard/>
           </div>
         </div>
         <div>
           <h2>Completed</h2>
-          <div className="kanban-column">
+          <div className="kanban-column kanban-scroll">
             <TaskCard/>
           </div>
         </div>
