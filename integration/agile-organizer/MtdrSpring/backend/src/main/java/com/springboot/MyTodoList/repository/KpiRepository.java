aab7b56592c353fb8ae7f882375149a7a2884f6b
sofia-lpz <a01028251@tec.mx>
2025-05-13 20:21:30 -0600

    more fixes for summary

--- integration/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/KpiRepository.java
+++ integration/agile-organizer/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/KpiRepository.java
@@ -25,7 +25,7 @@ public interface KpiRepository extends JpaRepository<Kpi,Integer> {
            "(coalesce(:teamIds, null) IS NULL OR t.user.team.id IN :teamIds) AND " +
            "(coalesce(:projectIds, null) IS NULL OR t.project.id IN :projectIds) AND " +
            "(coalesce(:sprintIds, null) IS NULL OR t.sprint.id IN :sprintIds)")
-    Optional<List<Kpi>> getKpiSummary(
+    List<Kpi> getKpiSummary(
             @Param("userIds") List<Integer> userIds,
             @Param("teamIds") List<Integer> teamIds,
             @Param("projectIds") List<Integer> projectIds,
