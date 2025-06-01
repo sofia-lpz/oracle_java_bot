ef15b0c538cb5814e3c5ade186f80cd5f4662720
sofia-lpz <a01028251@tec.mx>
2025-05-31 18:27:18 -0600

    commented out tests

--- integration/agile-organizer/MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/KpiControllerTest.java
+++ integration/agile-organizer/MtdrSpring/backend/src/test/java/com/springboot/MyTodoList/controller/KpiControllerTest.java
@@ -35,11 +35,6 @@ public class KpiControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    hola
-
-    JSKHDFHKSAGVH
-    hola hola HOLA HIOALM HAII SJAHDJAVHJKDK SVQHJFK VDSGAJ IJGDSUIH
-
     @Test
     public void testGetAllKpis() {
         // Arrange
